4858c3fea010a6dbaa8b12993a99bfb8285eb1ac
Yousif Jamal <yjamal1214@gmail.com>
2023-09-01 22:23:37 -0400

    Implemented form mailing to email

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,12 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  const body = JSON.stringify(req.body)
+import sendgrid from "@sendgrid/mail";
+
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+export default async function handler(req, res) {
+  const temp = JSON.stringify(req.body)
 
   // Optional logging to see the responses
   // in the command line where next.js app is running.
-  console.log('body: ', body)
+  console.log('body: ', temp)
+  const body = JSON.parse(temp)
+  const name = `${body.name}`
+  const email = `${body.email}`
+  const phone = `${body.phone}`
+  const description = `${body.description}`
+
+  const msg = {
+    to: "trollsapprentice@gmail.com", // Your email where you'll receive emails
+    from: "trolls.apprentice@gmail.com", // your website email address here
+    subject: "New Client",
+    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nNeed: ${description}`,
+    html: `<div>Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nNeed: ${description}</div>`,
+  }
+  
+
+  sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
 
 
-  res.status(200).json({ data: `${body}` })
+  res.status(200).json({ name,email })
 }
