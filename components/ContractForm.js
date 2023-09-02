4817e643608f28e8662558c209c8ef5344670e4b
Yousif Jamal <yjamal1214@gmail.com>
2023-09-02 11:22:14 -0400

    Updated for mobile responsitivity v3

--- components/ContractForm.js
+++ components/ContractForm.js
@@ -20,7 +20,7 @@ function ContractForm() {
         {/**<div className='absolute bottom-0 top-0 left-0 right-0 h-[160vh] sm:h-[120vh] bg-black/70 z-[2]' />**/}
             <div className='flex-row'>
                 <h1 className='text-5xl sm:text-5xl text-center text-white/75 mt-5 mb-10 font-serif'>CONTRACT US</h1>
-                  <div className='w-[80vw] h-[75vh] sm:h-[82vh] sm:w-[30vw] font-serif bg-gradient-to-tl from-slate-900 to-gray-500 justify-start items-start  sm:mt-5 sm:ml-20 text-black/75 shadow-2xl rounded-2xl p-10'>
+                  <div className='w-[80vw] h-[79vh] sm:h-[82vh] sm:w-[30vw] font-serif bg-gradient-to-tl from-slate-900 to-gray-500 justify-start items-start  sm:mt-5 sm:ml-20 text-black/75 shadow-2xl rounded-2xl p-10'>
                   <Form onSubmit={onSubmit} action='/api/mail' method='post'>
                     <h1 className='text-white/75 text-xl mb-2'>Name</h1>
                     <Input name="name" placeholder="Full Name" className='flex mb-10 w-[50vw] sm:w-[15vw] text-l rounded-md p-1' />
